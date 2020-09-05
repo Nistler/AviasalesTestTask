617189b3eafe40c60badfb9a5c04498fe8473ecb
Nistler <evgensin@gmail.com>
2020-09-06 01:12:23 +0300

    adding a mobile version

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import classNames from 'classnames';
 import { getTickets, generateKey } from './functions.js'
 import './App.css';
 import Ticket from './Ticket.jsx';
@@ -6,24 +7,45 @@ import Ticket from './Ticket.jsx';
 export default class App extends Component {
   constructor(props) {
     super(props);
-    this.state = { tickets: null };
+    this.state = { tickets: null, isLoading: true, isMenuHiden: true };
+  }
+
+  componentDidMount() {
+    this.getRequest();
   }
 
   getRequest = async () => {
+    this.setState({ isLoading: true });
     const responseID = await fetch('https://front-test.beta.aviasales.ru/search');
     const searchId = await responseID.json();
     const url = `https://front-test.beta.aviasales.ru/tickets?searchId=${searchId.searchId}`;
     const tickets = await getTickets(url, []);
-    this.setState({ tickets });
+    this.setState({ tickets, isLoading: false });
+  }
+
+  toggleMenu = () => {
+    const { isMenuHiden } = this.state;
+    this.setState({ isMenuHiden: !isMenuHiden });
   }
 
   render() {
-    const { tickets } = this.state;
+    const { tickets, isLoading, isMenuHiden } = this.state;
+    const mobileFilter = classNames({
+      filter: true,
+      mobileFilter: !isMenuHiden
+    });
+
+    const mobileMenu = classNames({
+      mobileMenu: true,
+      mobileMenuOpened: !isMenuHiden
+    });
+
     return (
       <div id="container">
-        <div className="filter">
+        <button className={mobileMenu} onClick={this.toggleMenu} ></button>
+        <div className={mobileFilter}>
           <div className="title">Количество пересадок</div>
-          <div className="checkboxes">
+          <form className="checkboxes">
             <div className="checkboxField">
               <input id="all" type="checkbox" />
               <label htmlFor="all">Все</label>
@@ -44,7 +66,7 @@ export default class App extends Component {
               <input id="threeTransfers" type="checkbox" />
               <label htmlFor="threeTransfers">3 пересадки</label>
             </div>
-          </div>
+          </form>
           <button onClick={this.getRequest} style={{width: 232 + 'px', marginTop: 30 + 'px'}}>Execute GET-request</button>
         </div>
         <div className="ticketsBlock">
@@ -53,7 +75,7 @@ export default class App extends Component {
             <button className="rightButton">Самый быстрый</button>
           </div>
           <div className="tickets">
-            {tickets && tickets
+            {isLoading ? <div className="donut"></div> : tickets
             .filter((ticket) => ticket.segments[0].stops.length === 0 && ticket.segments[1].stops.length === 0)
             .slice(0, 5)
             .map((ticket) => <Ticket key={generateKey()} ticket={ticket} />)}
