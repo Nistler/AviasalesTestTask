0d63201373e7346c7b072930f17728ef67457af5
Nistler <evgensin@gmail.com>
2020-09-12 23:00:30 +0300

    Updates

---
+++ precache-manifest.f1768bdf37f5350f91f1b50a2b56ee32.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a2c22fbdfdaaf9cef4d42cab37bd0db",
+    "url": "/AviasalesTestTask/index.html"
+  },
+  {
+    "revision": "7c10b95174a9e8b2b327",
+    "url": "/AviasalesTestTask/static/css/main.b7e7548e.chunk.css"
+  },
+  {
+    "revision": "101628517ad3753241f0",
+    "url": "/AviasalesTestTask/static/js/2.70c35b5a.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/AviasalesTestTask/static/js/2.70c35b5a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7c10b95174a9e8b2b327",
+    "url": "/AviasalesTestTask/static/js/main.bc0e0e47.chunk.js"
+  },
+  {
+    "revision": "665f5ad77f262574a09a",
+    "url": "/AviasalesTestTask/static/js/runtime-main.ce97d8aa.js"
+  }
+]);
